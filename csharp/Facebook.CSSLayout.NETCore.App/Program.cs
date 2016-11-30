88f26ec76a40e30f786a2d4a8c57d74245bdeb9d
Andrew Forster <anforste@bluejeansnet.com>
2016-11-30 13:36:32 -0800

    Playing with NETCore

---
+++ csharp/Facebook.CSSLayout.NETCore.App/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Facebook.CSSLayout.NETCore.App
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CSSNode parent = new CSSNode();
+            CSSNode child = new CSSNode();
+
+            parent.Insert(0, child);
+
+            parent.RemoveAt(0);
+        }
+    }
+}
